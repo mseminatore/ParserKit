d8932ad0fc2dbbe784a01411152c07dcb8a169aa
Mark Seminatore <mseminatore@users.noreply.github.com>
2019-05-24 14:49:24 -0700

    edits

--- examples/bnf/bnfparser.h
+++ examples/bnf/bnfparser.h
@@ -5,15 +5,31 @@
 class BNFParser : public BaseParser
 {
 protected:
+	enum class SymbolType
+	{
+		Terminal,
+		Nonterminal
+	};
+
+	struct Symbol {
+		SymbolType type;
+		std::string name;
+	};
+
 	// 
 	using RuleList = std::vector<int>;
 	using Rules = std::map<std::string, RuleList>;
 	Rules rules;
 
-	// the list of tokens
+	using Production = std::map<std::string, std::vector<Symbol>>;
+	using Productions = std::vector<Production>;
+
+	// the list of terminals
 	using TokenList = std::vector<std::string>;
 	TokenList tokens;
-	
+
+	std::vector<int> nullable;
+
 	void OutputTokens();
 
 public:
